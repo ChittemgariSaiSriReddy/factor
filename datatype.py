c38c8ea0cfae0f2ad6e4d48b0f41b9b39337c0e1
ChittemgariSaiSriReddy <chittemgarisaisri@gmail.com>
2024-02-10 20:27:00 +0530

    first commit

---
+++ datatype.py
@@ -0,0 +1,15 @@
+# integer
+a="kothi"
+print(type(a))
+# tuple
+b=(1,2,3)
+print(type(b))
+# list
+c=[23,21,56]
+print(type(c))
+c.append(45)
+print(c)
+c.remove(21)
+print(c)
+b.append(67)
+print(b)
